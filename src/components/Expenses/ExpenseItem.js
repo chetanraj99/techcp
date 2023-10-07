ad88ad8050999d66e3984d772a90a5583791fbd5
rpc1752 <rpc1752@gmail.com>
2023-10-07 16:00:10 +0530

    updated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,31 +1,37 @@
-import './ExpenseItem.css';
-import React, {useState} from "react";
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-const ExpenseItem=(props)=>{
-    
-    // const [Newtitle, setNewTitle]=useState("hi");
-    // const [title, setTitle]=useState(props.title);
+import "./ExpenseItem.css";
+import React from "react";
+import Card from "../UI/Card";
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
+const ExpenseItem = (props) => {
+	// const [Newtitle, setNewTitle]=useState("hi");
+	// const [title, setTitle]=useState(props.title);
 
-    // const clickHandler=()=>{
-    //         setTitle(Newtitle);
-    // }
+	// const clickHandler=()=>{
+	//         setTitle(Newtitle);
+	// }
 
-    // const changeHandler=(event)=>{
-    //     setNewTitle(event.target.value);
-    // }
-    
-    return (
-        <Card className='expense-item'>
-            {/* <ExpenseDate date={props.date}/> */}
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-            {/* <input type='text' value={Newtitle} onChange={changeHandler}/> */}
-            {/* <button onClick={clickHandler}>change Title</button> */}
-        </Card>
-    );
-}
+	// const changeHandler=(event)=>{
+	//     setNewTitle(event.target.value);
+	// }
+
+	return (
+		<Card className="expense-item">
+			{/* <ExpenseDate date={props.date}/> */}
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
+				<div className="btns">
+					<button>
+						<FiEdit />
+					</button>
+					<button>
+						<MdDelete />
+					</button>
+				</div>
+			</div>
+		</Card>
+	);
+};
 
 export default ExpenseItem;
